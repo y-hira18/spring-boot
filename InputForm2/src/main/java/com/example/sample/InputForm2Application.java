a3d9480eb99f9a0d81f5a73c599715f43e082af1
tinolwmf <tinolwm.f@docomo.ne.jp>
2019-04-06 19:10:07 +0900

    プロジェクトを作成

---
+++ InputForm2/src/main/java/com/example/sample/InputForm2Application.java
@@ -0,0 +1,13 @@
+package com.example.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InputForm2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InputForm2Application.class, args);
+	}
+
+}
